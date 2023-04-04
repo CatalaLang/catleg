946778147eed894e0fca6cb649f000995c5d7646
PRIMET Romain <romain.primet@inria.fr>
2023-04-04 10:24:02 +0200

    relocate ArticleType, and article ID parsing

--- tests/test_catala_parsing.py
+++ tests/test_catala_parsing.py
@@ -1,6 +1,7 @@
-from catala_devtools_fr.parse_catala_markdown import ArticleType, _parse_article_id
+from catala_devtools_fr.article import ArticleType, parse_article_id
+
 
 def test_parse_article_id():
-    typ, id = _parse_article_id("legiarti000038814944")
+    typ, id = parse_article_id("legiarti000038814944")
     assert typ == ArticleType.LEGIARTI
     assert id == "legiarti000038814944"
