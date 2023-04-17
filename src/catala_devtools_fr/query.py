99cf55ee8fb5be87a9ec662b68694f07a17a4890
PRIMET Romain <romain.primet@inria.fr>
2023-04-17 18:22:40 +0200

    introduce backend object

--- src/catala_devtools_fr/query.py
+++ src/catala_devtools_fr/query.py
@@ -9,7 +9,7 @@ import datetime
 import json
 import logging
 from types import SimpleNamespace
-from typing import Optional, Protocol
+from typing import List, Optional, Protocol
 
 import httpx
 
@@ -17,13 +17,53 @@ from catala_devtools_fr.article import Article, ArticleType, parse_article_id
 from catala_devtools_fr.config import settings
 
 
-def query_article(id: str) -> Optional[Article]:
-    # TODO: configurable backends (legistix...)
-    # and fallbacks (e.g. query legistix but use
-    # Legifrance for CETATEXT records)
-    reply = _query_article_legifrance(id)
-    article = _article_from_legifrance_reply(reply)
-    return article
+class Backend(Protocol):
+    def query_article(self, id: str) -> Optional[Article]:
+        ...
+
+    def query_articles(self, ids: List[str]) -> List[Article]:
+        ...
+
+
+class LegifranceBackend:
+    def __init__(self, client_id, client_secret):
+        self.client = httpx.Client(auth=LegifranceAuth(client_id, client_secret))
+
+    def query_article(self, id: str) -> Optional[Article]:
+        reply = self._query_article_legi(id)
+        article = _article_from_legifrance_reply(reply)
+        return article
+
+    def _query_article_legi(self, id: str):
+        typ, id = parse_article_id(id)
+        headers = {"Accept": "application/json"}
+        api_base_url = (
+            "https://api.aife.economie.gouv.fr/dila/legifrance-beta/lf-engine-app"
+        )
+        match typ:
+            case ArticleType.LEGIARTI | ArticleType.JORFARTI:
+                url = f"{api_base_url}/consult/getArticle"
+                params = {"id": id}
+            case ArticleType.CETATEXT:
+                url = f"{api_base_url}/consult/juri"
+                params = {"textId": id}
+            case _:
+                raise ValueError("Unknown article type")
+
+            # A POST request to fetch an article?
+            # And no way of using a simple query string?
+            # Really, Legifrance?
+        reply = self.client.post(url, headers=headers, json=params)
+        reply.raise_for_status()
+        return json.loads(reply.text)
+
+
+def get_backend(spec: str):
+    # XXX TODO: multiple backends, fallbacks...
+    client_id = settings.get("client_id")
+    client_secret = settings.get("client_secret")
+    _check_nonempty_legifrance_credentials(client_id, client_secret)
+    return LegifranceBackend(client_id, client_secret)
 
 
 def _query_article_legistix(id: str):
@@ -36,36 +76,6 @@ def _check_nonempty_legifrance_credentials(client_id, client_secret):
             "Please supply Legifrance credentials \
                          (using .catdev_secrets.toml or in the environment)"
         )
-
-
-def _query_article_legifrance(id: str, legifrance_args=None):
-    client_id = settings.get("client_id")
-    client_secret = settings.get("client_secret")
-    _check_nonempty_legifrance_credentials(client_id, client_secret)
-
-    typ, id = parse_article_id(id)
-    headers = {"Accept": "application/json"}
-    api_base_url = (
-        "https://api.aife.economie.gouv.fr/dila/legifrance-beta/lf-engine-app"
-    )
-    match typ:
-        case ArticleType.LEGIARTI | ArticleType.JORFARTI:
-            url = f"{api_base_url}/consult/getArticle"
-            params = {"id": id}
-        case ArticleType.CETATEXT:
-            url = f"{api_base_url}/consult/juri"
-            params = {"textId": id}
-        case _:
-            raise ValueError("Unknown article type")
-
-    # TODO: reuse client
-    with httpx.Client(auth=LegifranceAuth(client_id, client_secret)) as client:
-        # A POST request to fetch an article?
-        # And no way of using a simple query string?
-        # Really, Legifrance?
-        reply = client.post(url, headers=headers, json=params)
-        reply.raise_for_status()
-        return json.loads(reply.text)
 
 
 def _article_from_legifrance_reply(reply) -> Optional[Article]:
@@ -130,4 +140,5 @@ class LegifranceAuth(httpx.Auth):
 
 if __name__ == "__main__":
     """Example use"""
-    print(query_article("LEGIARTI000038814944"))
+    back = get_backend("legifrance")
+    print(back.query_article("LEGIARTI000038814944"))
