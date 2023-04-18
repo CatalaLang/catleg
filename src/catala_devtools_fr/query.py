6bf07d72a154b57504daf0b84e78e70dc689d492
PRIMET Romain <romain.primet@inria.fr>
2023-04-18 10:32:19 +0200

    introduce aiometer, switch requests to async, port sequential version
    add parallel requests
    
    add smoke-tests / simple integration tests

--- src/catala_devtools_fr/query.py
+++ src/catala_devtools_fr/query.py
@@ -5,12 +5,15 @@ Utilities for querying various data sources for law texts:
   - legistix database (auto-api via datasette)
 """
 
+import asyncio
 import datetime
+import functools
 import json
 import logging
 from types import SimpleNamespace
-from typing import List, Optional, Protocol
+from typing import Iterable, Optional, Protocol
 
+import aiometer
 import httpx
 
 from catala_devtools_fr.article import Article, ArticleType, parse_article_id
@@ -19,25 +22,30 @@ from catala_devtools_fr.config import settings
 
 
 class Backend(Protocol):
-    def query_article(self, id: str) -> Optional[Article]:
+    async def query_article(self, id: str) -> Optional[Article]:
         ...
 
-    def query_articles(self, ids: List[str]) -> List[Article]:
+    async def query_articles(self, ids: Iterable[str]) -> Iterable[Optional[Article]]:
         ...
 
 
 class LegifranceBackend(Backend):
     def __init__(self, client_id, client_secret):
-        self.client = httpx.Client(auth=LegifranceAuth(client_id, client_secret))
+        self.client = httpx.AsyncClient(auth=LegifranceAuth(client_id, client_secret))
 
-    def query_article(self, id: str) -> Optional[Article]:
-        reply = self._query_article_legi(id)
+    async def query_article(self, id: str) -> Optional[Article]:
+        reply = await self._query_article_legi(id)
         article = _article_from_legifrance_reply(reply)
         if article is None:
             logging.warning(f"Could not retrieve article {id} (wrong identifier?)")
         return article
 
-    def _query_article_legi(self, id: str):
+    async def query_articles(self, ids: Iterable[str]) -> Iterable[Optional[Article]]:
+        jobs = [functools.partial(self._query_article_legi, id) for id in ids]
+        replies = await aiometer.run_all(jobs, max_at_once=10, max_per_second=15)
+        return [_article_from_legifrance_reply(reply) for reply in replies]
+
+    async def _query_article_legi(self, id: str):
         typ, id = parse_article_id(id)
         headers = {"Accept": "application/json"}
         api_base_url = (
@@ -56,25 +64,27 @@ class LegifranceBackend(Backend):
         # A POST request to fetch an article?
         # And no way of using a simple query string?
         # Really, Legifrance?
-        reply = self.client.post(url, headers=headers, json=params)
+        reply = await self.client.post(url, headers=headers, json=params)
         reply.raise_for_status()
         return json.loads(reply.text)
 
 
 def get_backend(spec: str):
     # TODO: multiple backends, fallbacks...
-    client_id = settings.get("client_id")
-    client_secret = settings.get("client_secret")
-    _check_nonempty_legifrance_credentials(client_id, client_secret)
+    client_id, client_secret = _get_legifrance_credentials(raise_if_missing=True)
     return LegifranceBackend(client_id, client_secret)
 
 
-def _check_nonempty_legifrance_credentials(client_id, client_secret):
-    if (client_id is None) or (client_secret is None):
-        raise ValueError(
-            "Please supply Legifrance credentials \
+def _get_legifrance_credentials(*, raise_if_missing=True):
+    client_id = settings.get("lf_client_id")
+    client_secret = settings.get("lf_client_secret")
+    if raise_if_missing:
+        if (client_id is None) or (client_secret is None):
+            raise ValueError(
+                "Please supply Legifrance credentials \
                          (using .catdev_secrets.toml or in the environment)"
-        )
+            )
+    return client_id, client_secret
 
 
 def _article_from_legifrance_reply(reply) -> Optional[Article]:
@@ -102,6 +112,10 @@ class LegifranceAuth(httpx.Auth):
     Requires a client id and a client secret, uses those
     to fetch and refresh an access token and passes it along
     in requests.
+
+    Note that the auth flow is synchronous, this is on purpose
+    (we do not expect frequent token refreshes).
+    See https://github.com/encode/httpx/issues/1176#issuecomment-674381420
     """
 
     def __init__(self, client_id: str, client_secret: str):
@@ -141,4 +155,4 @@ if __name__ == "__main__":
     """Example use"""
     set_basic_loglevel()
     back = get_backend("legifrance")
-    print(back.query_article("LEGIARTI000038814944"))
+    print(asyncio.run(back.query_article("LEGIARTI000038814944")))
