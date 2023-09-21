c252fcbcd24c62d44c21497360ca2fa8d9e79f85
PRIMET Romain <romain.primet@inria.fr>
2023-09-21 18:02:39 +0200

    formatting

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,10 +1,10 @@
-# Regression test for https://github.com/CatalaLang/catleg/issues/71
 from catleg.query import _article_from_legifrance_reply
 from catleg.skeleton import _formatted_atricle
 
 from .test_legifrance_queries import _json_from_test_file
 
 
+# Regression test for https://github.com/CatalaLang/catleg/issues/71
 def test_no_article_renumbering():
     article_json = _json_from_test_file("LEGIARTI000044983201.json")
     article = _article_from_legifrance_reply(article_json)
